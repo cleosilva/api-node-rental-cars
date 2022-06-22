71e827a73c4fe9a64ba4501830740da84e23459f
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-06-22 18:36:19 -0300

    Implementando o conceito de Repository e passando a responsabilidade do create para ele

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,27 @@
-import { Router } from "express";
-import { v4 as uuidV4 } from 'uuid'; 
+import { Router } from 'express';
+import { CategoryRepository } from '../repositories/CategoryRepository';
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name , description } = request.body;
 
-    const category = {
-        name,
-        description,
-        id: uuidV4()
+    const categoryAlreadyExists = categoryRepository.findByName(name);
+    
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category already exists!"});
     }
-    categories.push(category);
+
+    categoryRepository.create({name, description});
 
     return response.status(201).send();
 });
 
-export { categoriesRoutes };
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoryRepository.list();
+
+    return response.json(all);
+})
+
+export { categoriesRoutes }
